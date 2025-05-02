72439726baf04c321056a6f27fed93d7a232b0b5
shamyalmahmood <shamyalmehmood@gmail.com>
2025-05-02 18:19:51 +0500

    First half of the project, basic styling of the news cards

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Components/Navbar"
+import NewsBoard from "./Components/NewsBoard"
+
+function App() {
+
+  return (
+    <>
+      <Navbar/>
+      <NewsBoard/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/NewsBoard.jsx
@@ -0,0 +1,23 @@
+import { useState, useEffect } from "react";
+import NewsItem from "./NewsItem";
+
+const NewsBoard = () => {
+    const [articles, setArticles] = useState([]);
+
+    useEffect(() => {
+        let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${import.meta.env.VITE_API_KEY}`;
+        fetch(url).then(response => response.json()).then(data => setArticles(data.articles));
+    }, [])
+
+    return (
+        <>
+            <h2 className="text-center">Latest <span className="badge bg-danger">News</span></h2>
+
+            {articles.map((news, index)=> {
+                return <NewsItem key={index} title={news.title} description={news.description} src = {news.urlToImage} url = {news.url}/>
+            })}
+        </>
+    );
+}
+
+export default NewsBoard;
